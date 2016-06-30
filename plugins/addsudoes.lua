a56a6bc873bb70e5730063c4011117a14e73ff25
kamran <djkamranhm@gmail.com>
2016-06-30 21:46:26 +0430

    Update addsudoes.lua

--- plugins/addsudoes.lua
+++ plugins/addsudoes.lua
@@ -4,7 +4,7 @@ vardump(success)
 vardump(result)
 end
 local function run(msg, matches)
-local sudo = 85831686 --Put you id Here !
+local sudo = 191442464 --Put you id Here !
 local addsudo = 'user#id'..sudo
 local chat = get_receiver(msg)
 if is_momod(msg) then -- you can set it to is_owner(msg)
@@ -15,16 +15,16 @@ return {
 description = "Add my sudo with you",
 usage = {
 user = {
-" List Help For Sphero-Bot:",
-"Developed by : @Joker_admin_1",
+" List Help For telekorosh-Bot:",
+"Developed by : @kamranya",
 },
 moderator = {
 "!addsudo : invite Sudo to your group" },
 },
 patterns ={
-"^[~!/][Aa][Dd][Dd][Ss][uU][Dd][oO]$",
+"^[!/][Aa][Dd][Dd][Ss][uU][Dd][oO]$",
 "^[Aa][Dd][Dd][Ss][uU][Dd][oO]$",
-"^[~!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
+"^[!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
 "^[Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
 },
 run = run

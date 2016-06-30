7a194a88038c34eedb704579f747c9bd6a21c847
kamran <djkamranhm@gmail.com>
2016-06-30 22:13:29 +0430

    Update expirechat2.lua

--- plugins/expirechat2.lua
+++ plugins/expirechat2.lua
@@ -1,10 +1,10 @@
-do
+
 local function callback(extra, success, result)
 vardump(success)
 vardump(result)
 end
 local function run(msg, matches)
-local sudo = 85831686 --Put you id Here !
+local sudo = 191442464 --Put you id Here !
 local addsudo = 'user#id'..sudo
 local chat = get_receiver(msg)
 if is_momod(msg) then -- you can set it to is_owner(msg)
@@ -15,7 +15,7 @@ return {
 patterns = {
 "^GroupTimeEnd!:free$",
 "^[Aa][Dd][Dd][Ss][uU][Dd][oO]$",
-"^[~!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
+"^[!/][Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
 "^[Aa][Dd][Dd][Aa][Dd][Mm][Ii][Nn]$",
 },
 run = run
